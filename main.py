cf6332ad8019dc54652aaf36094de71d593ceb43
Kébir ALLADE <alladeabdou01@gmail.com>
2024-02-14 15:11:08 +0100

    fix(put): updated row returned back

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import List
 
 from database import ProductTemp, create_connex
 from fastapi import FastAPI, HTTPException
-from statements import d1, i1, q1, q3, q4, u1
+from statements import d1, i1, q1, q2, q3, q4, u1
 
 app = FastAPI(
     title = "CUSTOMED SNOWFLAKE API",
@@ -34,12 +34,30 @@ def get_one_data(id: int):
         conn.commit()
         data = cur.fetchone()
         if  data is not None:
-            return {"ID": id, "Name": data[0]}
+            return {"ID": id, "Name": data[0],"Price":float(data[1])}
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error while getting data: {str(e)}")
     finally:
         cur.close()
         conn.close()
+
+@app.get("/get-data/name/{name}")
+def get_by_name(name: str):
+    """Get data by name from the product table"""
+    conn, cur = create_connex()
+    try:
+        cur.execute(q2, (name,))
+        conn.commit()
+        data = cur.fetchall()
+        print(data)
+        if data:
+            return [{"ID": d[0] , "Name":d[1], "Price": d[2]}for d in data ]
+        else:
+            raise f"No product with this name: {name}"
+    finally:
+        cur.close()
+        conn.close()
+
 
 
 
@@ -78,13 +96,14 @@ def update_product(product: ProductTemp, Id: int):
     """Update a specific product by its ID number"""
     conn, cur = create_connex()
     try:
-        cur.execute(u1, (product.Name, product.Price))
+        print(u1, product.Name, product.Id)
+        cur.execute(u1, (product.Name,product.Price,product.Id))
         conn.commit()
+    # Pull data  from DB to check if it was updated correctly
         cur.execute(q3, (Id,))
-        conn.commit()
         d = cur.fetchall()
-        print(d)
-        ### A completer###
+        if d:
+            return d
     finally:
         cur.close()
         conn.close()
