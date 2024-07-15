1484fb9dcedd1743f309ee68d0f2e8987f926faf
kebir ALLADE <alladeabdou01@gmail.com>
2024-07-15 20:14:02 +0200

    Refactor codebase

--- src/main.py
+++ src/main.py
@@ -3,11 +3,13 @@ import os
 from typing import List
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 import uvicorn
-from src.database import ProductTemp, create_connex
 from fastapi import FastAPI, HTTPException
+from src.database import ProductTemp, create_connex
+from src.database import ProductTemp, create_connex
 from src.statements import d1, i1, q1, q2, q3, q4, u1
 
 # Configure logging
@@ -19,12 +21,12 @@ logging.basicConfig(
 )
 
 # Fetch the host and port from environment variables with default values
-host = os.getenv("host", "127.0.0.1")
-port = int(os.getenv("port", 8000))
+host = os.getenv("host")
+port = int(os.getenv("port"))
 
 app = FastAPI(
     title="CUSTOMED SNOWFLAKE API",
-    description="This is a product API for managing snowflake tables.",
+    description="This is a product API for managing Snowflake tables.",
 )
 
 
